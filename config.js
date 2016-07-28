033713941c2cb164db68934df835ec6d665e4782
ian morton <ian.happytuesdays@gmail.com>
2016-07-28 09:15:32 +0100

    Change config

--- config.js
+++ config.js
@@ -10,15 +10,14 @@
 module.exports = {
     // target viz configuration
     engine: {
-        viz: '192.168.1.23',
+        viz: '172.20.69.14',
         vport: 6100,
-        trio: '192.168.1.23',
+        trio: '10.65.4.96',
         tport: 6200
     },
 
 // chat socket configuration
     chatserver: {
-        port: 3456
+        port: 7788
     }
 }
-
